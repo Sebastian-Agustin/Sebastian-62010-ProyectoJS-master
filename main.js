04793689eebaa35633f54fb9bf0582bb9b0cb5b0
sebastian <sebastianagustinmino@gmail.com>
2024-06-25 20:04:28 -0300

    pre-entrega 4

--- main.js
+++ main.js
@@ -2,70 +2,44 @@ let confirmacion = confirm("Para seguir con la pagina es necesario aceptar los t
 while(confirmacion == false){
   confirmacion = confirm("Para seguir con la pagina es necesario aceptar los terminos y condiciones")
 }
-let nombreDelUsuario = prompt("Un gusto! Ingresa tu nombre Aqui!")
+let nombreDelUsuario = prompt("Ingresa tu nombre Aqui!")
 
 const bienvenida =(nombreDelUsuario) =>{
-  let saludo = alert("Bienvenido " + nombreDelUsuario + "\n" + "Nuestra tienda de ropa GUUST!!!"+ "\n"+ "La mejor ropa la encontras aca.!")
-  return saludo
+   alert("Bienvenido " + nombreDelUsuario + "\n" + "A nuestra tienda de ropa GUUST!!!"+ "\n"+ "La mejor ropa la encontras aca, sin duda!")
+  return 
 }
-bienvenida(nombreDelUsuario)
-const pregunta = prompt(nombreDelUsuario+"\n"+" Lo invitamos a mirar nuestros descuentos% en nuestra tienda!" +"\n"+"Escriba TIENDA si quiere ver nuestro catalogo de ropa")
-const productos=[]
-const agregarProducto = ()=>{
-  const id = prompt("Ingresa el nombre del producto")
-  const medida =prompt("ingresa el talle.")
-  const lugar =prompt("ingresa el lugar de envio")
-  const producto ={id:id,medida:medida,lugar:lugar}
-  productos.push(producto)
+bienvenida(nombreDelUsuario);//
+
+const pregunta = prompt(nombreDelUsuario+"\n"+" Lo invitamos a mirar nuestros descuentos% en nuestra tienda!" +"\n"+"Escriba TIENDA si quiere ver nuestro catalogo de ropa" + " Escriba salir en caso de que no quiera")
+const producto=[]
+
+  if(pregunta === "TIENDA"){
+    alert("Este es nuestro catálogo de ropa:\n1- Remeras\n2- Pantalones\n3- Buzos\n4- Gorras\n");
+  }else {
+    alert("Gracias por su visita!" + "Vuelva pronto!")
+  }
+
+
+while(pregunta !="TIENDA"){
+  pregunta= prompt("Porfavor ingresa nuevamente los datos, Escriba TIENDA SI quiere continuar")
 }
-const tienda = prompt("escriba tienda si quiere ver nuestras secciones de ropa ")
-while(pregunta=="TIENDA"){
   
-}
-
-// while(tienda =="tienda"){
-
-  
-//   }
-
-
-// let seccionDeRopa= prompt("Ingrese el numero del producto"+"\n"+"1-Remeras"+"\n"+"2-Pantalones"+"\n"+"3-Buzos"+"\n"+"4-Gorras")
-
-// if(seccionDeRopa =="1"){
-//   alert("Estan son las remeras en stock" + "\n"+"1-Remera De Gust "+"\n"+"2-Remera de Gojo "+ "\n"+ "3-Remera de Fullmetal Alchemist ")
-//  agregarProducto()
-//  }
-//  else if(seccionDeRopa=="2"){
-//    alert("Estos son los pantalones en stock"+"\n"+"Pantalón cargo" +"\n"+"Pantalón mom jean"+"\n")
-//    agregarProducto()
-//  }
-//  else if(seccionDeRopa=="3"){
-//   alert("Estos son los  en stock"+"\n"+"Oversize con Goku" +"\n"+"Buzo Oversize Baki"+"\n"+"Buzo Oversize Pokemon")
-//  agregarProducto()
-//  }else if(seccionDeRopa=="4"){
-//   alert("Estos son los  en stock"+"\n"+"Gorras Personalizadas" +"\n"+"Gorras Cerradas"+"\n")
-//   agregarProducto()
-//  }else{
-//   alert("Los datos que ingresaste no son los correctos")
-//  }
-
-
-// // for (const i of productos) {
-// //   alert("a continuacion los productos")
-// //     alert("estos son los productos " + i)
-// // }
-// // pregunta = confirm(nombreDelUsuario+" Desea ver nuevamente la tienda? " + "\n" + "Porfavor confirme")
-// // }
-
-
-
-
-
-
-  // const agregarProducto = ()=>{
-  //   const id = prompt("Ingresa el nombre del producto")
-  //   const medida =prompt("ingresa el talle.")
-  //   const lugar =prompt("ingresa el lugar de envio")
-  //   const producto ={id:id,medida:medida,lugar:lugar}
-  //   productos.push(producto)
-  // }
+let bandera = true
+while(bandera){
+  let nombreDelProducto = prompt("Ingrese el nombre del producto que busca")
+  let talle = prompt("ingresa la medida del producto que elegiste")
+  let lugar=prompt("Escribe el lugar de envio")
+  producto.push(nombreDelProducto)
+  producto.push(talle)
+  producto.push(lugar)
+  console.log(producto)
+  let confirmacionDeTienda = confirm("Desea continuar? ")
+  if(confirmacionDeTienda == true){
+    alert("Genial, sigamos agregando!")
+  }
+ else{
+    alert("Gracias por su compra, este el producto que usted selecciono "+nombreDelProducto +"\n"+"Este es el talle seleccionado "+ talle+"\n" +"Este es el lugar de envio "+ lugar)
+    bandera = false
+ }
+ }
+ 
