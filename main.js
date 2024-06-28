9fa6708ab7cec30961d9d83264422c3df36d6d10
sebastian <sebastianagustinmino@gmail.com>
2024-06-28 13:50:59 -0300

    Este es otro commit

--- main.js
+++ main.js
@@ -1,45 +1,42 @@
-let confirmacion = confirm("Para seguir con la pagina es necesario aceptar los terminos y condiciones")
-while(confirmacion == false){
-  confirmacion = confirm("Para seguir con la pagina es necesario aceptar los terminos y condiciones")
+let nombreDelUsuario = prompt("Hola bienvenido !" + "\n"+"Escribe tu nombre")
+// let edad = Number(prompt("Ingresa tu edad"))
+while(nombreDelUsuario == null){
+  nombreDelUsuario = prompt("Hola bienvenido !" + "\n"+"Escribe tu nombre")
 }
-let nombreDelUsuario = prompt("Ingresa tu nombre Aqui!")
-
-const bienvenida =(nombreDelUsuario) =>{
-   alert("Bienvenido " + nombreDelUsuario + "\n" + "A nuestra tienda de ropa GUUST!!!"+ "\n"+ "La mejor ropa la encontras aca, sin duda!")
+const saludarCliente = function(nombre){
+  alert("Bienvenido "+ nombre)
   return 
 }
-bienvenida(nombreDelUsuario);//
 
-const pregunta = prompt(nombreDelUsuario+"\n"+" Lo invitamos a mirar nuestros descuentos% en nuestra tienda!" +"\n"+"Escriba TIENDA si quiere ver nuestro catalogo de ropa" + " Escriba salir en caso de que no quiera")
-const producto=[]
+let saludo = saludarCliente(nombreDelUsuario)
 
-  if(pregunta === "TIENDA"){
-    alert("Este es nuestro catálogo de ropa:\n1- Remeras\n2- Pantalones\n3- Buzos\n4- Gorras\n");
-  }else {
-    alert("Gracias por su visita!" + "Vuelva pronto!")
-  }
+const productos = []
+ let confirmacionTienda = confirm("Desea ver nuestros productos en descuento% " + nombreDelUsuario+ " confirme")
+console.log(confirmacionTienda)
 
+while(confirmacionTienda == true){
+ alert("Seccion de pagina"+"\n"+"Remeras 20%"+"\n"+"Pantalones 50%"+"\n"+"Gorras 15%"+"\n"+"Buzos 30% ")
+ let preguntaAlUsuario =prompt("Desea agregar algun producto al carrito? "+ "\n"+"Escriba el nombre del producto que quiera agregar")
+ console.log(productos.push(preguntaAlUsuario))
+ console.log(productos)
 
-while(pregunta !="TIENDA"){
-  pregunta= prompt("Porfavor ingresa nuevamente los datos, Escriba TIENDA SI quiere continuar")
-}
+alert("Estos son los producto que agrego "+ productos +"\n")
+
+let elimanarProducto = prompt("Desea eliminar algun producto? Escriba Si o no" ) 
+if(elimanarProducto == "Si" || elimanarProducto == "si"){
+  productos.pop()
+  alert("Usted elimino del carrito el ultimo producto agregado" + productos)
+  console.log(productos)
+}else if(elimanarProducto =="No" || elimanarProducto =="no"){
+  alert("Perfecto! continuemos")
   
-let bandera = true
-while(bandera){
-  let nombreDelProducto = prompt("Ingrese el nombre del producto que busca")
-  let talle = prompt("ingresa la medida del producto que elegiste")
-  let lugar=prompt("Escribe el lugar de envio")
-  producto.push(nombreDelProducto)
-  producto.push(talle)
-  producto.push(lugar)
-  console.log(producto)
-  let confirmacionDeTienda = confirm("Desea continuar? ")
-  if(confirmacionDeTienda == true){
-    alert("Genial, sigamos agregando!")
-  }
- else{
-    alert("Gracias por su compra, este el producto que usted selecciono "+nombreDelProducto +"\n"+"Este es el talle seleccionado "+ talle+"\n" +"Este es el lugar de envio "+ lugar)
-    bandera = false
- }
- }
- //
+}else{
+  alert("error!, Los datos ingresados no son los correctos")
+}
+
+
+ confirmacionTienda = confirm("Desea continuar agregando productos? " +nombreDelUsuario+" "+ "confirme")
+
+}
+
+
