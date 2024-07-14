cda20f52e26c3e0a31955e509af19a3bd8aa0f5a
Sebas <140217279+Sebastian-Agustin@users.noreply.github.com>
2024-07-14 14:58:09 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,87 @@
+const productos =[
+    //seccion buzo
+    {
+      id:0,
+      img:"img/Productos/buzos/buzoChopita.webp",
+      nombre:"Buzo de chopita",
+      precio: 60000,
+      cantidad:1,
+    },
+    {
+      id:1,
+      img:"img/Productos/buzos/buzogoku.webp",
+      nombre:"Buzo de Goku",
+      precio: 60000,
+      cantidad:1
+    },
+    {
+        id:2,
+        img:"img/Productos/buzos/buzoGeto.webp",
+        nombre:"Buzo De Geto",
+        precio: 30000,
+        cantidad:1 
+    } ,//seccion short
+    {
+        id:3,
+        img:"img/Productos/short/shortErenTitan.webp",
+        nombre:"short de Eren",
+        precio: 10000,
+        cantidad:1 
+    },
+    {
+        id:4,
+        img:"img/Productos/short/shortbaki.webp",
+        nombre:"Short de Baki Hanma",
+        precio: 10000,
+        cantidad:1
+    },
+    {
+        id:5,
+        img:"img/Productos/short/shortattack.webp",
+        nombre:"Short de Attack On titan",
+        precio: 10000,
+        cantidad:1
+    },
+    { //seccion remeras
+        id:6,
+        img:"img/Productos/remeras/remeraFMB.webp",
+        nombre:"Remera De FullMettalAlquimist",
+        precio: 30000,
+        cantidad:1
+    },
+    {
+        id:7,
+        img:"/img/Productos/remeras/remeraHinata.webp",
+        nombre:"Remera de Hinata",
+        precio: 30000,
+        cantidad:1
+    },
+    {
+        id:8,
+        img:"img/Productos/remeras/remeraGojo.webp",
+        nombre:"Remera de Satoru Gojo",
+        precio: 30000,
+        cantidad:1
+    },
+    { //seccion gorras
+        id:9,
+        img:"img/Productos/gorras/gorritafantasmita.webp",
+        nombre:"Gorra con un Fantasmita",
+        precio: 20000,
+        cantidad:1
+    },
+    {
+        id:10,
+        img:"img/Productos/gorras/gorrahonguito.webp",
+        nombre:"Gorra con un Honguito",
+        precio: 13000,
+        cantidad:1
+    },
+    {
+        id:11,
+        img:"img/Productos/gorras/gorritamando.webp",
+        nombre:"Gorra con un mando",
+        precio: 15000,
+        cantidad:1
+    } 
+] 
